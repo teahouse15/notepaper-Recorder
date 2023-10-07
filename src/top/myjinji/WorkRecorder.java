a4ae4ebf640749a680162c11677cd99dffe7a286
teahouse15 <pasterziv@gmail.com>
2023-10-07 17:43:44 +0800

    MainApp Create!

---
+++ src/top/myjinji/WorkRecorder.java
@@ -0,0 +1,16 @@
+package top.myjinji;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class WorkRecorder extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
