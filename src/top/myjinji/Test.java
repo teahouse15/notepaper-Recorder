45b1b451820c2b5de8e4afe2c870b9d911820f59
茶屋 <pasterziv@gmail.com>
2023-10-09 22:17:53 +0800

    Delete src/top/myjinji directory
    😫 删除原有

--- src/top/myjinji/Test.java
+++
@@ -1,9 +0,0 @@
-package top.myjinji;
-
-public class Test {
-
-    public static void main(String[] args) {
-        String rootPath2 = System.getProperty("user.dir");
-        System.out.println("Root Path (System.getProperty()): " + rootPath2);
-    }
-}
