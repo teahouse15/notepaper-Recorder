748e9a47b9c435560edaf9350d1b91485fca43a1
teahouse15 <pasterziv@gmail.com>
2023-10-12 22:55:49 +0800

    解决整体布局&鼠标拖拽

--- src/main/java/top/myjinji/WorkRecorder.java
+++ src/main/java/top/myjinji/WorkRecorder.java
@@ -11,12 +11,11 @@ import java.io.IOException;
 
 public class WorkRecorder extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+    private double xOffset = 0;
+    private double yOffset = 0;
 
     @Override
-    public void start(Stage stage) {
+    public void start(Stage primaryStage) {
         try {
 
             // 从fxml文件中读取Parent(布局)
@@ -28,15 +27,34 @@ public class WorkRecorder extends Application {
 
             scene.getStylesheets().add(WorkRecorder.class.getResource("/fxml/css/main.css").toExternalForm());
 
+            // 设置窗口可以使用鼠标拖拽
+            root.setOnMousePressed(event -> {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+                System.out.println("点击窗口");
+            });
+            root.setOnMouseDragged(event -> {
+                primaryStage.setX(event.getScreenX() - xOffset);
+                primaryStage.setY(event.getScreenY() - yOffset);
+                System.out.println("窗口被移动");
+                System.out.println(event.getSceneX());
+                System.out.println(event.getSceneY());
+            });
+
+
             // 窗口配置
-            stage.setResizable(false);
+            primaryStage.setResizable(false);
             // 设置无边框
-            stage.initStyle(StageStyle.UNDECORATED);
-            stage.setScene(scene);
-            stage.setTitle("Your Title");
-            stage.show();
+            primaryStage.initStyle(StageStyle.UNDECORATED);
+            primaryStage.setScene(scene);
+            primaryStage.setTitle("Your Title");
+            primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
